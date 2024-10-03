96d90fe0a95e1a68fc908af6738cba7a1e48ce10
Sandeep <singh29july95@gmail.com>
2024-10-03 17:57:09 +0530

    Encaplution code

---
+++ oops/src/encapsulation/Student.java
@@ -0,0 +1,22 @@
+package encapsulation;
+
+/**
+ * Author: SANDEEP
+ * Date: 03/10/24
+ */
+
+public class Student {
+
+    private String name;
+    private int age;
+    private double salary;
+    private String address;
+
+    public void setStudent(String n, int a,double s,String aa){
+        name =n;
+        age =a;
+        salary =s;
+        address =aa;
+    }
+
+}
---
+++ oops/src/encapsulation/UseStudent.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+/**
+ * Author: SANDEEP
+ * Date: 03/10/24
+ */
+
+public class UseStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setStudent("Sandeep",27,60.00,"Banglore");
+    }
+}
